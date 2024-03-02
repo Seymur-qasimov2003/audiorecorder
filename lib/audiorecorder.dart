2b7c1f696b618a579ec42373ea49f5930106b0d2
s.qasimov <seymur.qasimov@btarge.com>
2024-03-03 02:42:21 +0400

    last commit

--- lib/audiorecorder.dart
+++ lib/audiorecorder.dart
@@ -155,7 +155,7 @@ class _AudioRecorderPageState extends State<AudioRecorderPage> {
                                 ),
                               ),
                       ),
-                      title: Text('Audio ${index + 1}'),
+                      title: Text('Audioo ${index + 1}'),
                     );
                   },
                 ),
